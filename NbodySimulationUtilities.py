51267ef7642959ed4f4f5ae5d01b873843226e62
Samuel Hadden <shadden1107@gmail.com>
2020-05-05 14:26:55 -0400

    allow user to specify coordinates in get_simarchive_results

--- NbodySimulationUtilities.py
+++ NbodySimulationUtilities.py
@@ -14,14 +14,36 @@ def set_min_distance(sim,rhillfactor):
         mindist = rhillfactor * rhill
         sim.exit_min_distance = mindist
 
-def get_simarchive_integration_results(sa):
+def get_simarchive_integration_results(sa,coordinates='Jacobi'):
+    """
+    Read a rebound simulation archive and return a dictionary
+    with planets' orbital elements at each snapshot.
+
+    Arguments
+    ---------
+    sa : rebound.SimulationArchive (or reboundx.SimulationArchive)
+        The simulation archive from which to read results
+
+    coordinates : str
+        The coordinate system to use when determining orbital elements.
+        Can be 'Jacobi', 'heliocentric' or 'barycentric'.
+
+    Returns
+    -------
+    dict :
+        Dictionary containing simulation results. Dictionary entries
+        contain (N,M) array of orbital elements where N is the number
+        of planets and M is the number of snapshots in the simulation
+        archive. Dictionary mlso contains length M arrays 'time' and
+        'Energy' that record the time and system energy at each snapshot.
+    """
     if type(sa) == rb.simulationarchive.SimulationArchive:
-        return _get_rebound_simarchive_integration_results(sa)
+        return _get_rebound_simarchive_integration_results(sa,coordinates)
     elif type(sa) == rbx.simulationarchive.SimulationArchive:
-        return _get_reboundx_simarchive_integration_results(sa)
+        return _get_reboundx_simarchive_integration_results(sa,coordinates)
     raise TypeError("{} is not a rebound or reboundx simulation archive!".format(sa))
 
-def _get_rebound_simarchive_integration_results(sa):
+def _get_rebound_simarchive_integration_results(sa,coordinates):
     N = len(sa)
     sim0 = sa[0]
     Npl= sim0.N_real - 1
@@ -40,7 +62,16 @@ def _get_rebound_simarchive_integration_results(sa):
     }
     for i,sim in enumerate(sa):
         sim_results['time'][i] = sim.t
-        orbits= sim.calculate_orbits(jacobi_masses=True)
+
+        if coordinates=='Jacobi':
+            orbits= sim.calculate_orbits(jacobi_masses=True)
+        elif coordinates=='heliocentric'
+            orbits = sim.calculate_orbits(primary=sim.particles[0])
+        elif coordinates == 'barycentric':
+            orbits = sim.calculate_orbits(primary = sim.calculate_com())
+        else:
+            raise ValueError("'{}' is not a valid coordinate system!".format(coordinates))
+
         sim_results['Energy'][i] = sim.calculate_energy()
         for j,orbit in enumerate(orbits):
             sim_results['P'][j,i] = orbit.P
@@ -73,7 +104,14 @@ def _get_reboundx_simarchive_integration_results(sa):
     for i,sim_extra in enumerate(sa):
         sim,extra = sim_extra
         sim_results['time'][i] = sim.t
-        orbits= sim.calculate_orbits(jacobi_masses=True)
+        if coordinates=='Jacobi':
+            orbits= sim.calculate_orbits(jacobi_masses=True)
+        elif coordinates=='heliocentric'
+            orbits = sim.calculate_orbits(primary=sim.particles[0])
+        elif coordinates == 'barycentric':
+            orbits = sim.calculate_orbits(primary = sim.calculate_com())
+        else:
+            raise ValueError("'{}' is not a valid coordinate system!".format(coordinates))
         sim_results['Energy'][i] = sim.calculate_energy()
         for j,orbit in enumerate(orbits):
             sim_results['P'][j,i] = orbit.P
